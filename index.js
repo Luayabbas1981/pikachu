f7fd56c86770d8a74f68ad0b7598440607dae6b1
Luay Abbas <abbasluay1981@gmail.com>
2023-05-01 18:47:52 +0200

    colors

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const frameArr = Array.from(document.querySelectorAll(".frame > div:not(.div35) 
 const btn = frameArr[34]
 
 let colors=["#f44336","#e91e63","#9c27b0","#673ab7","#2196f3","#0390a2","#009688",
-"#ec8d01","#c73000","#795548","#7c7b7b"]
+"#ec8d01","#c73000","#795548","#7c7b7b","#607d8b","#7cb539","#922801","#3a9a3e",
+"#2a3ca1","#32aee5","#d0b007","#e5c100"]
 
     frameArr.map((item)=>{
         let x = Math.floor(Math.random()* 20)+1
