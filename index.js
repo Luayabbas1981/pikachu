b454c4b829e27ec0783cdb26d8b1b305c3f0a238
Luay Abbas <abbasluay1981@gmail.com>
2023-05-01 13:44:25 +0200

    color

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const frame = document.querySelector(".frame")
 const frameArr = Array.from(document.querySelectorAll(".frame > div "))
 
 let colors=["#f44336","#e91e63","#9c27b0","#673ab7","#2196f3","#00bcd4","#009688",
-"#ff9800","#c73000","#795548","#607d8b"]
+"#ff9800","#c73000","#795548","#9e9e9e"]
 
    
     frameArr.map((item)=>{
         let x = Math.floor(Math.random()* 20)+1
         let y = Math.floor(Math.random()* 20)+1
-       item.style=`--mainColor:#9e9e9e ; --x:${x + "px"} ; --y:${y + "px"} `
+       item.style=`--mainColor:#607d8b ; --x:${x + "px"} ; --y:${y + "px"} `
     } )
     
 
